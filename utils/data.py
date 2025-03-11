3bde86594f041063e2392b999c9e724a4d644fef
chinmaydk99 <chinmay.dk@gmail.com>
2025-03-11 15:38:35 -0700

    Changes to data.py

--- utils/data.py
+++ utils/data.py
@@ -12,8 +12,8 @@ class ViTFeatureExtractorTransforms:
 
         if feature_extractor.do_resize:
             # if pip install transformers == 4.18.0 does not work
-            # transform.append(Resize([feature_extractor.size['height'], feature_extractor.size['width']]))
-            transform.append(Resize([feature_extractor.size, feature_extractor.size]))
+            transform.append(Resize([feature_extractor.size['height'], feature_extractor.size['width']]))
+            # transform.append(Resize([feature_extractor.size, feature_extractor.size]))
 
         transform.append(ToTensor())
 
